46e53baa2338e70d977e1555e6f5d8d6af7db5d6
unknown <martinrpetersen171@gmail.com>
2019-10-29 14:08:48 -0300

    ATT

--- src/main/java/br/com/managebot/DemoApplication.java
+++ src/main/java/br/com/managebot/DemoApplication.java
@@ -1,10 +1,7 @@
 package br.com.managebot;
 
-import br.com.managebot.bot.ManageBot;
-import com.pengrad.telegrambot.TelegramBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -12,10 +9,4 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-    @Bean
-    public TelegramBot telegramBot() {
-        return new ManageBot();
-    }
-
 }
